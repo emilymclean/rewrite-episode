23a9036f411598c9db22f8316e686042b94a91e3
Emily McLean <emilymcleandeveloper@gmail.com>
2024-11-02 15:13:54 +1100

    Handle dots

--- rewrite-episode.sh
+++ rewrite-episode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Function to left-pad numbers
+# Function to left-pad numbers as two digits, handling non-octal numbers
 pad() {
     printf "%02d" "$((10#$1))"
 }
@@ -9,7 +9,7 @@ pad() {
 rename_file() {
     local file="$1"
     local title_option="$2"
-
+    
     # Extract the directory and filename
     local dir
     dir=$(dirname "$file")
@@ -22,11 +22,20 @@ rename_file() {
         episode=$(pad "${BASH_REMATCH[3]}")
         new_name="S${season}E${episode}"
         
-        # If title option is passed, attempt to extract the title
-        if [[ $title_option == "--title" ]]; then
+        # Determine the title parsing method based on the option
+        if [[ $title_option == "--dash-separated-title" ]]; then
+            # Use dash separator by default
             if [[ $base_name =~ [sS][0-9]+[eE][0-9]+\ -\ ([^()]+)\( ]]; then
                 title="${BASH_REMATCH[1]}"
                 title=$(echo "$title" | sed 's/[[:space:]]*$//')
+                new_name="${new_name} - ${title}"
+            fi
+        elif [[ $title_option == "--dot-separated-title" ]]; then
+            # Parse title with dot separator and stop at any digit
+            if [[ $base_name =~ [sS][0-9]+[eE][0-9]+\.((\.?[^.0-9]+)+)\.[0-9]+ ]]; then
+                title="${BASH_REMATCH[1]}"
+                echo "$title"
+                title=$(echo "$title" | sed 's/\./ /')
                 new_name="${new_name} - ${title}"
             fi
         fi
@@ -34,8 +43,7 @@ rename_file() {
         # Get file extension
         extension="${file##*.}"
         
-        
-        # Rename file
+        # Rename file within its directory
         mv "$file" "${dir}/${new_name}.${extension}"
         echo "Renamed '$base_name' to '${new_name}.${extension}'"
     else
@@ -51,6 +59,6 @@ if [[ -d "$1" ]]; then
 elif [[ -f "$1" ]]; then
     rename_file "$1" "$2"
 else
-    echo "Usage: $0 <file_or_directory> [--title]"
+    echo "Usage: $0 <file_or_directory> [--dash-separated-title | --dot-separated-title] --dry_run"
     exit 1
 fi
